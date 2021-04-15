c8330c52f52a29adea9b31645fee6386ba041d9b
nhatloi <1724801030179@student.tdmu.edu.vn>
2021-04-15 12:19:35 +0700

    update

--- client/src/components/AdminPage/Body/commons/AddNewReview.js
+++ client/src/components/AdminPage/Body/commons/AddNewReview.js
@@ -2,88 +2,15 @@ import React,{useState,useEffect} from 'react'
 import { Checkbox,Select ,message,Modal} from 'antd';
 import axios from 'axios'
 import {useSelector} from 'react-redux'
+import { Editor } from '@tinymce/tinymce-react';
 
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-ClassicEditor.builtinPlugins.map( plugin =>console.log( plugin.pluginName) );
+import 'suneditor/dist/css/suneditor.min.css'; // Import Sun Editor's CSS File
+var useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 
-ClassicEditor.defaultConfig = {
-    
-   
-
-     toolbar: {
-        items: [
-            'heading',
-            '|',
-            'alignment',                                                 // <--- ADDED
-            'bold',
-            'italic',
-            'link',
-            'bulletedList',
-            'numberedList',
-            'uploadImage',
-            'blockQuote',
-            'undo',
-            'redo',
-        ]
-    },
-
-    toolbar: {
-        items: [
-            'heading',
-            '|',
-            'alignment',                                                 // <--- ADDED
-            'bold',
-            'italic',
-            'link',
-            'bulletedList',
-            'numberedList',
-            'uploadImage',
-            'blockQuote',
-            'undo',
-            'redo',
-        ]
-    },
-    image: {
-        toolbar: [
-            'imageStyle:full',
-            'imageStyle:side',
-            '|',    
-            'imageTextAlternative'
-        ]
-    },
-
-    ckfinder: {
-
-        // Upload the images to the server using the CKFinder QuickUpload command.
-        uploadUrl: '/api/img',
-        
-    },
-    autosave: {
-        save( editor ) {
-            return saveData( editor.getData() );
-        }
-    }
-
-};
-
-function saveData( data ) {
-    console.log(data)
-}
-
-function displayStatus( editor ) {
-    const pendingActions = editor.plugins.get( 'PendingActions' );
-    const statusIndicator = document.querySelector( '#editor-status' );
-
-    pendingActions.on( 'change:hasAny', ( evt, propertyName, newValue ) => {
-        if ( newValue ) {
-            statusIndicator.classList.add( 'busy' );
-        } else {
-            statusIndicator.classList.remove( 'busy' );
-        }
-    } );
-}
+const handleEditorChange = (content, editor) =>{
+    console.log(content)
+  }
 
 const {Option} = Select;
 
@@ -219,21 +146,72 @@ function AddNewReview(props) {
                 >
 
 
-                    <CKEditor
-                        editor={ ClassicEditor }
+<Editor 
+ apiKey="p716pphziwt1dlm81kbh5sylhmy8jfkrr9d6yk5vf6ckw4dj"
+ init={{
+    selector: 'textarea#full-featured-non-premium',
+  plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+  imagetools_cors_hosts: ['picsum.photos'],
+  menubar: 'file edit view insert format tools table help',
+  toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+  toolbar_sticky: true,
+  autosave_ask_before_unload: true,
+  autosave_interval: '30s',
+  autosave_prefix: '{path}{query}-{id}-',
+  autosave_restore_when_empty: false,
+  autosave_retention: '2m',
+  image_advtab: true,
+  link_list: [
+    { title: 'My page 1', value: 'https://www.tiny.cloud' },
+    { title: 'My page 2', value: 'http://www.moxiecode.com' }
+  ],
+  image_list: [
+    { title: 'My page 1', value: 'https://www.tiny.cloud' },
+    { title: 'My page 2', value: 'http://www.moxiecode.com' }
+  ],
+  image_class_list: [
+    { title: 'None', value: '' },
+    { title: 'Some class', value: 'class-name' }
+  ],
+  importcss_append: true,
+  file_picker_callback: function (callback, value, meta) {
+    /* Provide file and text for the link dialog */
+    if (meta.filetype === 'file') {
+      callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
+    }
 
-                        data="<p>Hello from the first editor working with the context!</p>"
-                        onReady={ editor => {
-                            // You can store the "editor" and use when it is needed.
-                            console.log( 'Editor1 is ready to use!', editor );
-                        } }
+    /* Provide image and alt text for the image dialog */
+    if (meta.filetype === 'image') {
+      callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
+    }
 
-                        onChange={ ( event, editor ) => {
-                            const data = editor;
-                            console.log( { event, editor, data } );
-                        } }
+    /* Provide alternative source and posted for the media dialog */
+    if (meta.filetype === 'media') {
+      callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+    }
+  },
+  templates: [
+        { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+    { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+    { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+  ],
+  template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+  template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+  height: 600,
+  image_caption: true,
+  quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+  noneditable_noneditable_class: 'mceNonEditable',
+  toolbar_mode: 'sliding',
+  contextmenu: 'link image imagetools table',
+  skin: useDarkMode ? 'oxide-dark' : 'oxide',
+  content_css: useDarkMode ? 'dark' : 'default',
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  
+}}
 
-                    />
+    onEditorChange={handleEditorChange}
+
+/>
               
                 </Modal>
         </div>
